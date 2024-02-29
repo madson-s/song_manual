462daa96f0d86f1d4fdf8670f0180810ca94e4e8
Madso Silva <madson@Madsos-iMac-Pro.local>
2024-02-29 12:17:36 -0300

    feat: move pages data to dataSet file

---
+++ dataset.ts
@@ -0,0 +1,73 @@
+import Escola from "./assets/Fabula1.png";
+import BeABa from "./assets/Fabula2.png";
+import A from "./assets/Fabula3.png";
+import SomDoP from "./assets/Fabula4.png";
+import Maninha from "./assets/Fabula5.png";
+
+
+export const dataSet = [
+  {
+    image: {
+      alt: "Escola",
+      src: Escola,
+    },
+    song: {
+      title: "AO CHEGAR NA MINHA ESCOLA",
+      lyrics:
+        "Ao chegar na minha escola <br />Meu bom dia vou contar <br />Meu bom bom dia caro mestre <br />Vamos todos trabalhar <br />Ao chegar na minha escola <br />Meu bom dia vou contar <br />Aos queridos coleguinhos <br />Desta fâmia salutar.",
+    },
+    color: "lime",
+  },
+
+  {
+    image: {
+      alt: "BeABa",
+      src: BeABa,
+    },
+    song: {
+      title: "BE-A-BA",
+      lyrics:
+        "O ba-be-bi-bo-bu <br />Vamos todos aprender <br />Soletrando o be-a-ba <br />Uma das letras do ABC<br />O b é uma das letras <br />Que se encontra no ABC <br />Luis você não sabe <br />Quanto eu gosto de você",
+    },
+    color: "green",
+  },
+
+  {
+    image: {
+      alt: "A",
+      src: A,
+    },
+    song: {
+      title: "A",
+      lyrics:
+        "A Ana tem uma trança<br />Muito fácil de trançar <br />Ela põe um lindo laço <br />E vai contente passear",
+    },
+    color: "emerald",
+  },
+
+  {
+    image: {
+      alt: "SomDoP",
+      src: SomDoP,
+    },
+    song: {
+      title: "O SOM DA LETRA P",
+      lyrics:
+        "Olha lá, seu Sarafim<br />Esta letrinha faz assim <br />Com a ela faz pa <br />Com e ela faz pe <br /> Com i ela faz pi <br />Com o ela faz po <br /> Com u ela faz pu <br /> Pa pe pi po pu <br /> Pa pe pi po pu",
+    },
+    color: "teal",
+  },
+
+  {
+    image: {
+      alt: "Maninha",
+      src: Maninha,
+    },
+    song: {
+      title: "Maninha",
+      lyrics:
+        "Vamos maninha, vamos<br />Juntinhos escrever <br />As palavras e as frases <br />Para a meninada ler",
+    },
+    color: "cyan",
+  },
+];
